76f3152ebb99106d7f89dd7a27923fc881f0476e
rks12174 <rks12174@gmail.com>
2026-03-17 09:31:27 +0000

    feat: initial commit or update of current work

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-// Add JS here
+const lottoNumbersContainer = document.querySelector('.lotto-numbers');
+const generateBtn = document.querySelector('.generate-btn');
+
+const generateNumbers = () => {
+    lottoNumbersContainer.innerHTML = '';
+    const numbers = new Set();
+    while (numbers.size < 6) {
+        const randomNumber = Math.floor(Math.random() * 45) + 1;
+        numbers.add(randomNumber);
+    }
+
+    for (const number of numbers) {
+        const numberElement = document.createElement('span');
+        numberElement.textContent = number;
+        lottoNumbersContainer.appendChild(numberElement);
+    }
+};
+
+generateBtn.addEventListener('click', generateNumbers);
